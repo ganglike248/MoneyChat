a549849818abf1009d3635e015710877e6d148ee
ganglike <rudfkr248@gmail.com>
2024-12-14 01:16:05 +0900

    v1.0.1

--- moneychat-frontend/src/chatbot/config.js
+++ moneychat-frontend/src/chatbot/config.js
@@ -5,7 +5,7 @@ import LoadingSpinner from '../components/LoadingSpinner';
 const config = {
   initialMessages: [
     createChatBotMessage(
-      "안녕하세요! 저는 당신의 지출 관리를 도와드릴 '머니챗'입니다. 오늘 지출하신 내용을 편하게 알려주세요!",
+      "안녕하세요! 저는 당신의 지출 관리를 도와드릴 '머니챗'입니다. 오늘 지출하신 내용을 편하게 알려주세요! \n\n*첫 대화 요청 시 1~2분 정도 지연이 발생할 수 있습니다.*",
       {
         widget: "options",
       }
